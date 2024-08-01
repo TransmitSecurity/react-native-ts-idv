5063f3998426f1bc4c794d5a788e692f31551435
Shachar Udi <shachar@transmitsecurity.com>
2024-08-01 10:48:38 -0400

    MOB-836 #comment Fixed code review issues

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -53,7 +53,7 @@ export default class App extends React.Component<any, State> {
     verificationResultsResponse: null,
     errorMessage: "",
     isProcessing: false,
-    lastVerificationSessionID:  null // "BeSltmsId6gMzPm"
+    lastVerificationSessionID:  null
   }
 
   componentDidMount(): void {
